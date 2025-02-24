5daeee63afe205bfab8e97ee18d1bd2aed83bfce
47bahadir <bahadir.alixir@outlook.com>
2025-02-25 04:37:06 +0800

    Site updated: 2025-02-25 04:37:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/23/post2/","2025/02/22/post1/"];function toRandomPost(){
+var posts=["2025/02/22/post1/","2025/02/23/post2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
