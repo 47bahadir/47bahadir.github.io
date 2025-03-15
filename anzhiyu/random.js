f3e8cf48db472ebaf1aa0536454e83209133a3ec
47bahadir <bahadir.alixir@outlook.com>
2025-03-15 17:15:04 +0800

    Site updated: 2025-03-15 17:15:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/23/post2/","2025/03/08/post5/","2025/02/22/post1/","2025/03/04/post4/","2025/02/26/post3/","2025/03/14/post6/"];function toRandomPost(){
+var posts=["2025/02/23/post2/","2025/02/22/post1/","2025/02/26/post3/","2025/03/04/post4/","2025/03/14/post6/","2025/03/08/post5/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
